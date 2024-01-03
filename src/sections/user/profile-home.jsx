b9f0af7d103fa2e7eb24c163400baa3b9e3309e2
Adel <adelaaymann@gmail.com>
2024-01-03 16:47:27 +0300

    working on uploding imges foe users

--- src/sections/user/profile-home.jsx
+++ src/sections/user/profile-home.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Unstable_Grid2';
@@ -5,145 +6,354 @@ import Typography from '@mui/material/Typography';
 import ListItemText from '@mui/material/ListItemText';
 import { fDate } from 'src/utils/format-time';
 import Iconify from 'src/components/iconify';
+import Divider from '@mui/material/Divider';
 import { useAuthContext } from 'src/auth/hooks';
-
+import Image from 'src/components/image/image';
 // ----------------------------------------------------------------------
 
 export default function ProfileHome() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  function calculateAge(birthDate) {
+    const today = new Date();
+    const dob = new Date(birthDate);
+
+    const age = today.getFullYear() - dob.getFullYear();
+    return age;
+  }
   const { user } = useAuthContext();
   const renderContent = (
     <Stack component={Card} spacing={3} sx={{ p: 3 }}>
-      <Typography variant="h4">
-        {user.patient.first_name} {user.patient.last_name}
-      </Typography>
-
       {user.patient.drug_allergies.length > 0 && (
         <Stack spacing={2}>
-          <Typography style={{ fontWeight: 600 }} variant="h6">
-            Drug Allergies
+          <Typography style={{ color: 'gray' }} variant="body1">
+            <Iconify
+              style={{ color: 'rgb(0,156,0)', position: 'relative', left: '-3px', top: '2px' }}
+              icon="guidance:no-drug-or-substance"
+              />{' '}
+              Drug Allergies{' '}
           </Typography>
           <Stack spacing={1}>
             {user.patient.drug_allergies?.map((drug) => (
-              <li key={drug?._id}>{drug.trade_name}</li>
+              <li style={{ fontWeight: 500, fontSize: '17px', listStyle: 'none' }} key={drug?._id}>
+                {' '}
+                {drug.trade_name}
+              </li>
             ))}
           </Stack>
+          <Divider sx={{ borderStyle: 'dashed', borderColor: 'rgba(128, 128, 128, 0.512)' }} />
         </Stack>
       )}
       {user.patient.diseases?.length > 0 && (
         <Stack spacing={2}>
-          <Typography style={{ fontWeight: 600 }} variant="h6">
-            Diseases
+          <Typography style={{ color: 'gray' }} variant="body1">
+            <Iconify
+              style={{ color: 'rgb(0,156,0)', position: 'relative', left: '-3px', top: '2px' }}
+              icon="ph:virus"
+              />{' '}
+              Diseases{' '}
           </Typography>
           <Stack spacing={1}>
             {user.patient.diseases?.map((disease) => (
-              <li key={disease._id}>{disease.name_english}</li>
+              <li
+                style={{ fontWeight: 500, fontSize: '17px', listStyle: 'none' }}
+                key={disease._id}
+              >
+                {' '}
+                {disease.name_english}
+              </li>
             ))}
           </Stack>
+          <Divider sx={{ borderStyle: 'dashed', borderColor: 'rgba(128, 128, 128, 0.512)' }} />
         </Stack>
       )}
       {user.patient.surgeries.length > 0 && (
         <Stack spacing={2}>
-          <Typography style={{ fontWeight: 600 }} variant="h6">
-            Surgeries
+          <Typography style={{ color: 'gray' }} variant="body1">
+            <Iconify
+              style={{ color: 'rgb(0,156,0)', position: 'relative', left: '-3px', top: '2px' }}
+              icon="guidance:surgery"
+              />{' '}
+              Surgeries{' '}
           </Typography>
           <Stack spacing={1}>
             {user.patient.surgeries.map((surgery) => (
-              <li key={surgery._id}>{surgery.name}</li>
+              <li
+                style={{ fontWeight: 500, fontSize: '17px', listStyle: 'none' }}
+                key={surgery._id}
+              >
+                {' '}
+                {surgery.name}
+              </li>
             ))}
           </Stack>
+          <Divider sx={{ borderStyle: 'dashed', borderColor: 'rgba(128, 128, 128, 0.512)' }} />
         </Stack>
       )}
       {user.patient.medicines.length > 0 && (
         <Stack spacing={2}>
-          <Typography style={{ fontWeight: 600 }} variant="h6">
-            Medicines
+          <Typography style={{ color: 'gray' }} variant="body1">
+            <Iconify
+              style={{ color: 'rgb(0,156,0)', position: 'relative', left: '-3px', top: '2px' }}
+              icon="healthicons:medicines-outline"
+              />{' '}
+              Medicines{' '}
           </Typography>
           <Stack spacing={1}>
             {user.patient.medicines?.map((data) => (
-              <li key={data._id}>{data?.frequently}</li>
+              <li style={{ fontWeight: 500, fontSize: '17px', listStyle: 'none' }} key={data._id}>
+                {' '}
+                {data?.frequently}
+              </li>
             ))}
           </Stack>
+          <Divider sx={{ borderStyle: 'dashed', borderColor: 'rgba(128, 128, 128, 0.512)' }} />
         </Stack>
       )}
       {user.patient.insurance.length > 0 && (
         <Stack spacing={2}>
-          <Typography style={{ fontWeight: 600 }} variant="h6">
-            Insurance
+          <Typography style={{ color: 'gray' }} variant="body1">
+            <Iconify
+              style={{ color: 'rgb(0,156,0)', position: 'relative', left: '-3px', top: '2px' }}
+              icon="streamline:insurance-hand"
+              />{' '}
+              Insurance{' '}
           </Typography>
           <Stack spacing={1}>
             {user.patient.insurance.map((company) => (
-              <li key={company._id}>{company?.name_english}</li>
+              <li
+                style={{ fontWeight: 500, fontSize: '17px', listStyle: 'none' }}
+                key={company._id}
+              >
+                {company?.name_english}
+              </li>
             ))}
           </Stack>
+          <Divider sx={{ borderStyle: 'dashed', borderColor: 'rgba(128, 128, 128, 0.512)' }} />
         </Stack>
       )}
 
       {user.patient.sport_exercises && (
         <Stack spacing={2}>
-          <Typography style={{ fontWeight: 600 }} variant="h6">
-            Sport Exercises
+          <Typography style={{ color: 'gray' }} variant="body1">
+            <Iconify
+              style={{ color: 'rgb(0,156,0)', position: 'relative', left: '-3px', top: '2px' }}
+              icon="icon-park-outline:sport"
+              />{' '} 
+              Sport Exercises
           </Typography>
-          <li>{user.patient.sport_exercises}</li>
+          <li style={{ fontWeight: 500, fontSize: '17px', listStyle: 'none' }}>
+            {' '}
+            {user.patient.sport_exercises}
+          </li>
+          <Divider sx={{ borderStyle: 'dashed', borderColor: 'rgba(128, 128, 128, 0.512)' }} />
         </Stack>
       )}
       {user.patient.eating_diet && (
         <Stack spacing={2}>
-          <Typography style={{ fontWeight: 600 }} variant="h6">
-            Eating Diet
+          <Typography style={{ color: 'gray' }} variant="body1">
+            <Iconify
+              style={{ color: 'rgb(0,156,0)', position: 'relative', left: '-3px', top: '2px' }}
+              icon="fluent:food-apple-20-regular"
+              />{' '}
+              Eating Diet{' '}
           </Typography>
-          <li>{user.patient.eating_diet?.name_english}</li>
+          <li style={{ fontWeight: 500, fontSize: '17px', listStyle: 'none' }}>
+            {' '}
+            {user.patient.eating_diet?.name_english}
+          </li>
+          <Divider sx={{ borderStyle: 'dashed', borderColor: 'rgba(128, 128, 128, 0.512)' }} />
         </Stack>
       )}
       {user.patient.alcohol_consumption && (
         <Stack spacing={2}>
-          <Typography style={{ fontWeight: 600 }} variant="h6">
-            Alcohol Consumption
+          <Typography style={{ color: 'gray' }} variant="body1">
+            <Iconify
+              style={{ color: 'rgb(0,156,0)', position: 'relative', left: '-3px', top: '2px' }}
+              icon="healthicons:alcohol"
+              />{' '}
+              Alcohol Consumption{' '}
           </Typography>
-          <li>{user.patient.alcohol_consumption}</li>
+          <li style={{ fontWeight: 500, fontSize: '17px', listStyle: 'none' }}>
+            {' '}
+            {user.patient.alcohol_consumption}
+          </li>
+          <Divider sx={{ borderStyle: 'dashed', borderColor: 'rgba(128, 128, 128, 0.512)' }} />
         </Stack>
       )}
       {user.patient.smoking && (
         <Stack spacing={2}>
-          <Typography style={{ fontWeight: 600 }} variant="h6">
-            Smoking
+          <Typography style={{ color: 'gray' }} variant="body1">
+            <Iconify
+              style={{ color: 'rgb(0,156,0)', position: 'relative', left: '-3px', top: '2px' }}
+              icon="healthicons:smoking-outline"
+              />{' '}
+              Smoking
           </Typography>
-          <li>{user.patient.smoking}</li>
+          <li style={{ fontWeight: 500, fontSize: '17px', listStyle: 'none' }}>
+            {' '}
+            {user.patient.smoking}
+          </li>
+          <Divider sx={{ borderStyle: 'dashed', borderColor: 'rgba(128, 128, 128, 0.512)' }} />
         </Stack>
       )}
       {user.patient.other_medication_notes && (
         <Stack spacing={2}>
-          <Typography style={{ fontWeight: 600 }} variant="h6">
-            Other Medication Notes
+          <Typography style={{ color: 'gray' }} variant="body1">
+            <Iconify
+              style={{ color: 'rgb(0,156,0)', position: 'relative', left: '-3px', top: '2px' }}
+              icon="charm:notes"
+              />{' '}
+              Notes
           </Typography>
-          <li>{user.patient.other_medication_notes}</li>
+          {user.patient.other_medication_notes.map((note, index) => (
+            <li key={index} style={{ fontWeight: 500, fontSize: '17px', listStyle: 'none' }}>
+              {note}
+            </li>
+          ))}
         </Stack>
       )}
     </Stack>
   );
 
   const renderOverview = (
-    <Stack component={Card} spacing={2} sx={{ p: 3 }}>
+    <Stack component={Card} spacing={4} sx={{ p: 3, marginBottom: 2 }}>
+      <div>
+        <Image
+          alt="profile"
+          src={user?.patient.profile_picture}
+          sx={{
+            height: '150px',
+            width: '100px',
+            boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)',
+            // borderLeft: 2,
+            // borderColor: 'rgb(0, 156, 0)',
+            // borderRadius: '12px',
+          }}
+        />
+        <Iconify
+          icon="solar:pin-bold-duotone"
+          style={{
+            position: 'absolute',
+            top: 5,
+            left: 115,
+            height: '30px',
+            width: '30px',
+            color: 'rgb(0, 156, 0)',
+            zIndex: 1,
+          }}
+        />
+        <Typography variant="h4" sx={{ mt: 2 }}>
+          {user.patient.first_name} {user.patient.last_name}
+        </Typography>
+      </div>
       {[
-        // {
-        //   label: 'Patient Father',
-        //   value: patient_father?.userName,
-        //   icon: <Iconify icon="solar:calendar-date-bold" />,
-        // },
         {
           label: 'Gender',
           value: user.patient.gender,
           icon: <Iconify icon="solar:calendar-date-bold" />,
         },
-        // {
-        //   label: 'Birth Place',
-        //   value: nationality?.name_english,
-        //   icon: <Iconify icon="solar:calendar-date-bold" />,
-        // },
         {
-          label: 'Birth Date',
-          value: fDate(user.patient.birth_date),
+          label: 'Age',
+          value: calculateAge(user.patient.birth_date),
           icon: <Iconify icon="solar:calendar-date-bold" />,
         },
+        {
+          label: 'Height',
+          value: user.patient.height,
+          icon: <Iconify icon="solar:calendar-date-bold" />,
+        },
+        {
+          label: 'Weight',
+          value: user.patient.weight,
+          icon: <Iconify icon="solar:calendar-date-bold" />,
+        },
+      ].map((item) => (
+        <>
+          {item.value && (
+            <Stack key={item.label}>
+              <Typography sx={{ color: 'gray' }} variant="body1">
+                {item.label} :{' '}
+                <span
+                  style={{
+                    color: 'black',
+                    fontWeight: 500,
+                    fontSize: '17px',
+                    float: 'right',
+                    position: 'relative',
+                    right: '15px',
+                  }}
+                >
+                  {item.value}
+                </span>
+              </Typography>
+            </Stack>
+          )}
+        </>
+      ))}
+    </Stack>
+  );
+  const renderMoreInfo = (
+    <Stack component={Card} spacing={4} sx={{ p: 3, marginBottom: 2 }}>
+      {[
+        {
+          label: 'Identification Number',
+          value: user.patient.identification_num,
+          icon: <Iconify icon="solar:calendar-date-bold" />,
+        },
+        {
+          label: 'Blood Type',
+          value: user.patient.blood_type,
+          icon: <Iconify icon="solar:calendar-date-bold" />,
+        },
+        {
+          label: 'Marital Status',
+          value: user.patient.marital_status,
+          icon: <Iconify icon="solar:calendar-date-bold" />,
+        },
+        {
+          label: 'Address',
+          value: user.patient.address,
+          icon: <Iconify icon="solar:clock-circle-bold" />,
+        },
+        {
+          label: 'Mobile Number',
+          value: user.patient.mobile_num1,
+          icon: <Iconify icon="carbon:skill-level-basic" />,
+        },
+        {
+          label: 'Second Mobile Number',
+          value: user.patient.mobile_num2,
+          icon: <Iconify icon="carbon:skill-level-basic" />,
+        },
+      ].map((item) => (
+        <>
+          {item.value && (
+            <Stack key={item.label}>
+              <Typography sx={{ color: 'gray' }} variant="body1">
+                {item.label} :{' '}
+                <span
+                  style={{
+                    color: 'black',
+                    fontWeight: 500,
+                    fontSize: '16px',
+                    float: 'right',
+                    position: 'relative',
+                    right: '15px',
+                  }}
+                >
+                  {item.value}
+                </span>
+              </Typography>
+            </Stack>
+          )}
+        </>
+      ))}
+    </Stack>
+  );
+
+  const renderMoreInfoPregnant = (
+    <Stack component={Card} spacing={4} sx={{ p: 3, marginBottom: 2 }}>
+      {[
         {
           label: 'Identification Number',
           value: user.patient.identification_num,
@@ -160,16 +370,6 @@ export default function ProfileHome() {
           icon: <Iconify icon="solar:calendar-date-bold" />,
         },
         {
-          label: 'Height',
-          value: user.patient.height,
-          icon: <Iconify icon="solar:calendar-date-bold" />,
-        },
-        {
-          label: 'Weight',
-          value: user.patient.weight,
-          icon: <Iconify icon="solar:calendar-date-bold" />,
-        },
-        {
           label: 'Marital Status',
           value: user.patient.marital_status,
           icon: <Iconify icon="solar:calendar-date-bold" />,
@@ -179,11 +379,6 @@ export default function ProfileHome() {
           value: user.patient.address,
           icon: <Iconify icon="solar:clock-circle-bold" />,
         },
-        // {
-        //   label: 'Email',
-        //   value: email,
-        //   icon: <Iconify icon="solar:wad-of-money-bold" />,
-        // },
         {
           label: 'Mobile Number',
           value: user.patient.mobile_num1,
@@ -197,22 +392,22 @@ export default function ProfileHome() {
       ].map((item) => (
         <>
           {item.value && (
-            <Stack key={item.label} spacing={1.5}>
-              {/* {item.icon} */}
-              <ListItemText
-                primary={item.label}
-                secondary={item.value}
-                primaryTypographyProps={{
-                  typography: 'body2',
-                  color: 'text.secondary',
-                  mb: 0.5,
-                }}
-                secondaryTypographyProps={{
-                  typography: 'subtitle2',
-                  color: 'text.primary',
-                  component: 'span',
-                }}
-              />
+            <Stack key={item.label} spacing={2}>
+              <Typography sx={{ color: 'gray' }} variant="body1">
+                {item.label} :{' '}
+                <span
+                  style={{
+                    color: 'black',
+                    fontWeight: 600,
+                    fontSize: '17px',
+                    float: 'right',
+                    position: 'relative',
+                    right: '15px',
+                  }}
+                >
+                  {item.value}
+                </span>
+              </Typography>
             </Stack>
           )}
         </>
@@ -221,12 +416,13 @@ export default function ProfileHome() {
   );
   return (
     <Grid container spacing={3}>
-      <Grid xs={12} md={8}>
-        {renderContent}
-      </Grid>
-
       <Grid xs={12} md={4}>
         {renderOverview}
+        {user.patient.gender === 'male' ? [renderMoreInfo] : [renderMoreInfoPregnant]}
+      </Grid>
+
+      <Grid xs={12} md={8}>
+        {renderContent}
       </Grid>
     </Grid>
   );
