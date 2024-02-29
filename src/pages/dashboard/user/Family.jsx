e983444fb0406809016d195b46d7b01d1edd141f
Adel <adelaaymann@gmail.com>
2024-02-29 16:51:20 +0300

    family

---
+++ src/pages/dashboard/user/Family.jsx
@@ -0,0 +1,22 @@
+import { Helmet } from 'react-helmet-async';
+
+import { useTranslate } from 'src/locales';
+
+import { Family } from 'src/sections/user/view';
+
+// ----------------------------------------------------------------------
+
+export default function Fam() {
+  const { t } = useTranslate();
+
+  return (
+    <>
+      <Helmet>
+        <title> {t('Financial movements')} </title>
+        <meta name="description" content="meta" />
+      </Helmet>
+
+      <Family />
+    </>
+  );
+}
