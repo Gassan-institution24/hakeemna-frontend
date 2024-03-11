f373a6d2591792d30942782b2903d8e2a247192c
Adel <adelaaymann@gmail.com>
2024-03-11 16:29:27 +0300

    updates

--- src/sections/user/view/user-family-view.jsx
+++ src/sections/user/view/user-family-view.jsx
@@ -1,7 +1,14 @@
+import React, { useEffect } from 'react';
+
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
 import Container from '@mui/material/Container';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
 
 import { paths } from 'src/routes/paths';
+
+import { useBoolean } from 'src/hooks/use-boolean';
 
 import { useTranslate } from 'src/locales';
 
@@ -17,6 +24,37 @@ import Iconify from 'src/components/iconify';
 import Familymem from '../familymem';
 
 export default function Family() {
+  const dialog = useBoolean(false);
+
+  const yesfunction = async () => {
+    // try {
+    //   const response = await axios.patch(`/api/patient/${user?.patient?._id}/updateonboard`, {
+    //     is_onboarded: true,
+    //   });
+    //   setOldpatientsdata(response.data);
+    //   user.patient.is_onboarded = true;
+    //   enqueueSnackbar(`Please check your data`, { variant: 'success' });
+    // } catch (error) {
+    //   console.error('Error updating data:', error);
+    // }
+    // router.push(paths.dashboard.user.oldpatientdata);
+    alert('Sdsdsd');
+  };
+  const nofunction = async () => {
+    // try {
+    //   const response = await axios.patch(`/api/patient/${user?.patient?._id}/updateonboard`, {
+    //     is_onboarded: true,
+    //   });
+    //   setOldpatientsdata(response.data);
+    //   user.patient.is_onboarded = true;
+    //   enqueueSnackbar(`Thanks for your cooperation`, { variant: 'success' });
+    // } catch (error) {
+    //   console.error('Error updating data:', error);
+    // }
+    // dialog.onFalse();
+    alert('Sdsdsd');
+  };
+
   const settings = useSettingsContext();
   const { t } = useTranslate();
 
@@ -32,7 +70,7 @@ export default function Family() {
         action={
           <Button
             component={RouterLink}
-            href={paths.dashboard.user.new}
+            onClick={dialog.onTrue}
             variant="contained"
             startIcon={<Iconify icon="mingcute:add-line" />}
           >
@@ -42,7 +80,19 @@ export default function Family() {
         sx={{ mb: { xs: 3, md: 5 } }}
       />
 
-      <Familymem  />
+      <Dialog open={dialog.value} onClose={dialog.onTrue}>
+        <DialogTitle>Does the user have an account?</DialogTitle>
+        <DialogActions>
+          <Button href={paths.dashboard.user.profile} variant="outlined" color="success" type="submit">
+            {t('Yes')}
+          </Button>
+          <Button type="submit" variant="contained" color="inherit" href={paths.dashboard.user.profile}>
+            {t('No')}
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Familymem />
     </Container>
   );
 }
