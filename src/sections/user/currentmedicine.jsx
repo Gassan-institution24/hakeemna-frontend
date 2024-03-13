de5bc6b6993bd12df03bed1deb1bd6e8a897d921
AdelAlAtram <adelaaymann@gmail.com>
2024-03-13 19:17:33 +0300

    sdsds

---
+++ src/sections/user/currentmedicine.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { useGetUser } from 'src/api/user';
+import axiosHandler from 'src/utils/axios-handler';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useAuthContext } from 'src/auth/hooks';
+
+export default function ProfileFollowers() {
+  const { data } = useGetUser();
+  const [countdowns, setCountdowns] = useState({});
+
+  const { user } = useAuthContext();
+
+  // useEffect(() => {
+  //   if (data) {
+  //     const currentDate = new Date();
+  //     const countdownTimers = {};
+  //     data.current_medications.forEach((med) => {
+  //       const endDate = new Date(med.enddate);
+  //       if (endDate > currentDate) {
+  //         const intervalId = setInterval(() => {
+  //           const now = new Date();
+  //           const difference = endDate.getTime() - now.getTime();
+  //           const millisecondsInDay = 1000 * 60 * 60 * 24;
+  //           const daysDifference = Math.floor(difference / millisecondsInDay);
+  //           setCountdowns((prevCountdowns) => ({
+  //             ...prevCountdowns,
+  //             [med.name]: daysDifference > 0 ? daysDifference : 0,
+  //           }));
+  //         }, 1000);
+  //         countdownTimers[med.name] = intervalId;
+  //       }
+
+  //     });
+  //       Object.values(countdownTimers).forEach((timerId) =>  clearInterval(timerId));
+
+  //   }
+  // }, [data]);
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="right">Frequently</TableCell>
+              <TableCell align="right">Dose</TableCell>
+              <TableCell align="right">Duration</TableCell>
+              <TableCell align="right">Price</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* <TableRow key={} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell align="center">{}</TableCell>
+                <TableCell align="right">{}</TableCell>
+                <TableCell align="right"></TableCell>
+                <TableCell align="right"></TableCell>
+                <TableCell align="right"></TableCell>
+              </TableRow> */}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
---
+++ src/sections/user/patientappointment.jsx
@@ -0,0 +1,70 @@
+import { useState, useCallback } from 'react';
+import { useGetpatientAppointment } from 'src/api/user';
+// ----------------------------------------------------------------------
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useAuthContext } from 'src/auth/hooks';
+
+export default function ProfileFollowers() {
+  const { data } = useGetpatientAppointment();
+  const { user } = useAuthContext();
+  return (
+    <>
+      <h4 style={{ color: 'green' }}>table number 1</h4>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">name</TableCell>
+              <TableCell align="right">frequently</TableCell>
+              <TableCell align="right">dose</TableCell>
+              <TableCell align="right">duration</TableCell>
+            </TableRow>
+          </TableHead>
+          {/* 
+          <TableBody>
+            <TableRow key={data.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell component="th" scope="row">
+          
+              </TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
+          </TableBody> */}
+        </Table>
+      </TableContainer>
+      {/* مواعيد حاليه  */}
+      <h4 style={{ color: 'red' }}>table number 2</h4>
+      {/* مواعيد منتهية */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">name</TableCell>
+              <TableCell align="right">frequently</TableCell>
+              <TableCell align="right">dose</TableCell>
+              <TableCell align="right">duration</TableCell>
+            </TableRow>
+          </TableHead>
+
+          {/* <TableBody>
+            <TableRow key={} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell component="th" scope="row">
+                
+              </TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
+          </TableBody> */}
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
