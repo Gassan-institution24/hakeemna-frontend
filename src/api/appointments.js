c017e0e35316b9a35b614287634845e63b6c244e
Adel <adelaaymann@gmail.com>
2024-05-14 16:57:46 +0300

    working on the entrance managment

--- src/api/appointments.js
+++ src/api/appointments.js
@@ -111,6 +111,27 @@ export function useGetPatientOneAppointments(id) {
 
   return { ...memoizedValue, refetch };
 }
+export function useGetUsAppointmentsToday(id) {
+  const URL = endpoints.appointments.unit_service.today(id);
+
+  const { data, isLoading, error, isValidating } = useSWR(URL, fetcher);
+  const memoizedValue = useMemo(
+    () => ({
+      appointmentsData: data,
+      loading: isLoading,
+      error,
+      validating: isValidating,
+      empty: !isLoading && !data?.length,
+    }),
+    [data, error, isLoading, isValidating]
+  );
+  const refetch = async () => {
+    // Use the mutate function to re-fetch the data for the specified key (URL)
+    await mutate(URL);
+  };
+
+  return { ...memoizedValue, refetch };
+}
 
 export function useGetUSAppointments({ id, page, sortBy, rowsPerPage, order, filters }) {
   const URL = endpoints.appointments.unit_service.one({
