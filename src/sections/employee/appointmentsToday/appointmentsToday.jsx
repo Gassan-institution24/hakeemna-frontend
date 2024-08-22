9870b89b92e75ce5233ab4db274d8ef1134594d4
Adel <adelaaymann@gmail.com>
2024-08-22 16:21:48 +0300

    master

--- src/sections/employee/appointmentsToday/appointmentsToday.jsx
+++ src/sections/employee/appointmentsToday/appointmentsToday.jsx
@@ -38,7 +38,6 @@ import axiosInstance, { endpoints } from 'src/utils/axios';
 import { useAuthContext } from 'src/auth/hooks';
 import { useAclGuard } from 'src/auth/guard/acl-guard';
 import { useLocales, useTranslate } from 'src/locales';
-import { useAclGuard } from 'src/auth/guard/acl-guard';
 import {
   useGetUSRooms,
   useGetEntranceManagement,
