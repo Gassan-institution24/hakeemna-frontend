dc892cbec3b882e70897e5cb6d7c605daebfc1a1
Adel <adelaaymann@gmail.com>
2024-08-29 12:30:44 +0300

    updates

--- src/api/prescription.js
+++ src/api/prescription.js
@@ -66,3 +66,24 @@ export function useGePrescription(id) {
 
   return { ...memoizedValue, refetch };
 }
+export function useGetCurrentPatientMedicines(id) {
+  const URL = endpoints.prescription.current(id);
+
+  const { data, isLoading, error, isValidating } = useSWR(URL, fetcher);
+  const memoizedValue = useMemo(
+    () => ({
+      prescriptionData: data,
+      loading: isLoading,
+      error,
+      validating: isValidating,
+      empty: !isLoading && !data?.length,
+    }),
+    [data, error, isLoading, isValidating]
+  );
+  const refetch = async () => {
+    // Use the mutate function to re-fetch the data for the specified key (URL)
+    await mutate(URL);
+  };
+
+  return { ...memoizedValue, refetch };
+}
