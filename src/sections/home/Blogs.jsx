7abf0f77d0096c63311d12c73e4b1ef16dbd0810
Adel <adelaaymann@gmail.com>
2024-09-18 16:31:04 +0300

    faq and blogs

--- src/sections/home/Blogs.jsx
+++ src/sections/home/Blogs.jsx
@@ -1,43 +1,33 @@
 import * as Yup from 'yup';
 import * as React from 'react';
-
-import { Box, Card, Grid, Link, Stack, Button, Typography } from '@mui/material';
-
-import { useTranslate } from 'src/locales';
-
-import Iconify from 'src/components/iconify';
-import Image from 'src/components/image/image';
-
-import axiosInstance from 'src/utils/axios';
-
+import { useForm } from 'react-hook-form';
 import { enqueueSnackbar } from 'notistack';
 import { yupResolver } from '@hookform/resolvers/yup';
 
-import { useForm } from 'react-hook-form';
+import { Box, Card, Grid, Link, Stack, Button, Typography } from '@mui/material';
 
-import { RHFUpload, RHFTextField } from 'src/components/hook-form';
+import { paths } from 'src/routes/paths';
 
-import FormProvider from 'src/components/hook-form/form-provider';
+import axiosInstance from 'src/utils/axios';
+import { fDateAndTime } from 'src/utils/format-time';
 
 import { useGetBlogs } from 'src/api';
-import { paths } from 'src/routes/paths';
+import { useTranslate } from 'src/locales';
 import { useAuthContext } from 'src/auth/hooks';
+
+import Iconify from 'src/components/iconify';
+import Image from 'src/components/image/image';
+import FormProvider from 'src/components/hook-form/form-provider';
+import { RHFUpload, RHFTextField, RHFEditor } from 'src/components/hook-form';
+
+import BlogImage from './images/blog.png';
 
 export default function Blogs() {
   const { t } = useTranslate();
   const { user } = useAuthContext();
-  const { data } = useGetBlogs();
-  console.log(data);
+  const { data, refetch } = useGetBlogs();
 
-  const blogs = [
-    {
-      title: 'Blog 1',
-      topic:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quod vero odio ab debitis optio at sequi suscipit vitae obcaecati nobis, corrupti eaque beatae, odit possimus ex quaerat similique? Ad.',
-      link: 'www.dfdf;dfdfdfgergeg.com',
-      date: '20/11/2024',
-    },
-  ];
+  console.log(data);
 
   const [hoveredIndex, setHoveredIndex] = React.useState(null);
 
@@ -135,7 +125,7 @@ export default function Blogs() {
       await axiosInstance.post('/api/blogs', formData);
 
       enqueueSnackbar('blog uploaded successfully', { variant: 'success' });
-      // refetch();
+      refetch();
       reset();
     } catch (error) {
       if (error?.response?.status !== 401) {
@@ -151,6 +141,23 @@ export default function Blogs() {
         );
       }
     }
+  };
+  const formatTextWithLineBreaks = (text, id, limit = 20) => {
+    if (!text) return '';
+
+    const chunks = [];
+
+    for (let i = 0; i < text.length; i += 100) {
+      chunks.push(text.slice(i, i + 100));
+    }
+
+    let formattedText = chunks.join('<br />');
+
+    if (text.length > limit) {
+      formattedText = `${text.slice(0, limit)}...`;
+    }
+
+    return formattedText;
   };
 
   return (
@@ -170,16 +177,17 @@ export default function Blogs() {
             <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
               <RHFTextField name="title" placeholder={t('title')} sx={{ mb: 2 }} />
               <RHFTextField
-                name="topic"
-                placeholder={t('topic')}
-                sx={{ mb: 2, display: 'block' }}
-                multiline
-              />
-              <RHFTextField
                 name="link"
                 placeholder={t('Add link')}
                 sx={{ mb: 2, display: 'block' }}
               />
+              <RHFEditor
+                lang="en"
+                name="topic"
+                label={t('topic')}
+                sx={{ mb: 2, mt: 2, textTransform: 'lowercase' }}
+              />
+
               <RHFUpload
                 multiple
                 autoFocus
@@ -191,10 +199,6 @@ export default function Blogs() {
                 onRemove={handleRemoveFile}
                 onRemoveAll={handleRemoveAllFiles}
               />
-              {/* <Button
-                variant="contained"
-                sx={{ bgcolor: 'success.main', color: 'white', display: 'block' }}
-              > */}
               <Button type="submit" loading={isSubmitting} variant="contained">
                 {t('blog')}
               </Button>
@@ -236,10 +240,7 @@ export default function Blogs() {
                   onMouseEnter={() => handleMouseEnter(index)}
                   onMouseLeave={handleMouseLeave}
                 >
-                  <Image
-                    src="https://pbwebdev.co.uk/wp-content/uploads/2018/12/blogs.jpg"
-                    alt={blog.title}
-                  />
+                  <Image src={blog?.user?.picture || BlogImage} alt={blog.title} />
                   <Box
                     sx={{
                       p: 2,
@@ -248,7 +249,7 @@ export default function Blogs() {
                     }}
                   >
                     <Typography sx={{ color: 'gray' }}>{blog.title}</Typography>
-                    <Typography
+                    {/* <Typography
                       sx={{
                         mb: 3,
                         mt: 3,
@@ -258,7 +259,19 @@ export default function Blogs() {
                       }}
                     >
                       {blog.topic?.length > 50 ? `${blog.topic.substring(0, 50)}... ` : blog.topic}
-                    </Typography>
+                    </Typography> */}
+                    <Typography
+                      dangerouslySetInnerHTML={{
+                        __html: formatTextWithLineBreaks(blog.topic),
+                      }}
+                      sx={{
+                        mb: 3,
+                        mt: 3,
+                        fontWeight: 'bold',
+                        transition: 'all 0.5s ease-in-out',
+                        opacity: hoveredIndex === index ? 0 : 1,
+                      }}
+                    />
 
                     <Link
                       href={blog.link}
@@ -267,7 +280,6 @@ export default function Blogs() {
                         opacity: hoveredIndex === index ? 0 : 1,
                         fontSize: 13,
                       }}
-                      // onClick={() => alert('Coming soon')}
                     >
                       {blog.link}
                     </Link>
@@ -275,10 +287,9 @@ export default function Blogs() {
                     <Typography
                       sx={{ color: 'gray', opacity: hoveredIndex === index ? 0 : 1, mt: 2 }}
                     >
-                      {blog.date}
+                      {fDateAndTime(blog.created_at)}
                     </Typography>
                   </Box>
-                  {/* This box appears when hovered */}
                   {hoveredIndex === index && (
                     <Box
                       sx={{
@@ -290,10 +301,12 @@ export default function Blogs() {
                         bgcolor: 'rgba(0, 0, 0, 0.7)',
                         color: 'white',
                         display: 'flex',
+
                         alignItems: 'center',
                         justifyContent: 'center',
                         p: 2,
                         transition: 'all 0.5s ease-in-out',
+                        flexDirection: 'column',
                       }}
                     >
                       <Link
@@ -307,12 +320,34 @@ export default function Blogs() {
                           display: 'flex',
                           alignItems: 'center',
                           justifyContent: 'center',
+                          mb: 3,
                         }}
                         title={blog.link}
                         href={blog.link}
                         target="_blank"
                       >
                         {t('View')} <Iconify icon="mingcute:link-line" sx={{ ml: '1px' }} />
+                      </Link>
+                      <Link
+                        sx={{
+                          cursor: 'pointer',
+                          borderRadius: 1,
+                          p: 1,
+                          color: 'white',
+                          bgcolor: 'success.main',
+                          width: '90px',
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                        }}
+                        title={blog.user?.employee?.name_english}
+                        href={paths.pages.doctor(
+                          blog.user?.employee?.employee_engagements?.[
+                            user.employee.selected_engagement
+                          ]?._id
+                        )}
+                      >
+                        {t('profile')} <Iconify icon="iconamoon:profile-fill" sx={{ ml: '1px' }} />
                       </Link>
                     </Box>
                   )}
