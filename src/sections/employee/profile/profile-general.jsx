4fb7b7911c51bd6eebdb7326acdcf22971b12ac8
Mohammad_Zaytowna <mhd.keath@gmail.com>
2024-09-28 16:50:55 +0300

    companies

--- src/sections/employee/profile/profile-general.jsx
+++ src/sections/employee/profile/profile-general.jsx
@@ -181,8 +181,8 @@ export default function AccountGeneral({ employeeData, refetch }) {
     signature: employeeData?.signature || null,
     stamp: employeeData?.stamp || null,
     picture: employeeData?.picture || null,
-    about_me: decode(employeeData?.about_me) || '',
-    arabic_about_me: decode(employeeData?.arabic_about_me) || '',
+    about_me: employeeData?.about_me ? decode(employeeData?.about_me) : '',
+    arabic_about_me: employeeData?.arabic_about_me ? decode(employeeData?.arabic_about_me) : '',
     languages: employeeData?.languages || [],
     arabic_keywords: employeeData?.arabic_keywords || [],
     keywords: employeeData?.keywords || [],
@@ -193,12 +193,12 @@ export default function AccountGeneral({ employeeData, refetch }) {
     certifications: employeeData?.certifications?.length
       ? employeeData?.certifications
       : [
-          {
-            name: '',
-            institution: '',
-            year: null,
-          },
-        ],
+        {
+          name: '',
+          institution: '',
+          year: null,
+        },
+      ],
     fees: user?.employee?.employee_engagements?.[user.employee.selected_engagement]?.fees || 0,
     // currency:
     //   user?.employee?.employee_engagements?.[user.employee.selected_engagement]?.currency ||
@@ -387,7 +387,7 @@ export default function AccountGeneral({ employeeData, refetch }) {
                 variant="filled"
                 name="profrssion_practice_num"
                 label={`${t('profrssion practice number')} :`}
-                // value={values.profrssion_practice_num}
+              // value={values.profrssion_practice_num}
               />
               <TextField
                 // disabled
@@ -507,14 +507,14 @@ export default function AccountGeneral({ employeeData, refetch }) {
                 options={specialtiesData.map((speciality) => speciality._id)}
                 getOptionLabel={(option) =>
                   specialtiesData.find((one) => one._id === option)?.[
-                    curLangAr ? 'name_arabic' : 'name_english'
+                  curLangAr ? 'name_arabic' : 'name_english'
                   ]
                 }
                 renderOption={(props, option, idx) => (
                   <li lang="ar" {...props} key={idx} value={option}>
                     {
                       specialtiesData.find((one) => one._id === option)?.[
-                        curLangAr ? 'name_arabic' : 'name_english'
+                      curLangAr ? 'name_arabic' : 'name_english'
                       ]
                     }
                   </li>
@@ -540,14 +540,14 @@ export default function AccountGeneral({ employeeData, refetch }) {
                 options={employeeTypesData.map((one) => one._id)}
                 getOptionLabel={(option) =>
                   employeeTypesData.find((one) => one._id === option)?.[
-                    curLangAr ? 'name_arabic' : 'name_english'
+                  curLangAr ? 'name_arabic' : 'name_english'
                   ]
                 }
                 renderOption={(props, option, idx) => (
                   <li lang="ar" {...props} key={idx} value={option}>
                     {
                       employeeTypesData.find((one) => one._id === option)?.[
-                        curLangAr ? 'name_arabic' : 'name_english'
+                      curLangAr ? 'name_arabic' : 'name_english'
                       ]
                     }
                   </li>
