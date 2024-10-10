2ef7679dcf0ffca58716ad659a55db6f1bc47f8f
Adel <adelaaymann@gmail.com>
2024-10-10 11:55:15 +0300

    updates

--- src/sections/home/Blogs.jsx
+++ src/sections/home/Blogs.jsx
@@ -19,16 +19,20 @@ import { useRouter } from 'src/routes/hooks';
 import { fDateAndTime } from 'src/utils/format-time';
 
 import { useTranslate } from 'src/locales';
-import { useAuthContext } from 'src/auth/hooks';
 import { useGetBlogs, useGetBlog_category } from 'src/api';
 
 import Image from 'src/components/image/image';
 
 export default function Blogs() {
   const { t } = useTranslate();
-  const { data } = useGetBlogs();
+  const { data } = useGetBlogs({
+    populate: {
+      path: 'user',
+      select: 'employee',
+      populate: { path: 'employee', select: '_id name_english name_arabic employee_engagements' },
+    },
+  });
   const { Data } = useGetBlog_category();
-  const { user } = useAuthContext();
   const router = useRouter();
 
   const [searchTerm, setSearchTerm] = useState('');
@@ -53,46 +57,49 @@ export default function Blogs() {
   };
 
   // Handle search and filter
-  console.log(data)
-  console.log(filterBy)
   const filteredBlogs = data?.filter((blog) => {
     const matchesSearch =
       (blog.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        blog.topic.toLowerCase().includes(searchTerm.toLowerCase())) && (
-        filterBy ? blog.category === filterBy : true
-      )
+        blog.topic.toLowerCase().includes(searchTerm.toLowerCase())) &&
+      (filterBy ? blog.category === filterBy : true);
 
     return matchesSearch;
   });
 
   return (
-    <Stack sx={{ p: 5, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <Card sx={{ width: '70%' }}>
-        <Box sx={{ p: 5, textAlign: 'start' }}>
-          <Typography variant="h2" component="h2" sx={{ mb: 10, textAlign: 'center' }}>
+    <Stack
+      sx={{ p: { xs: 2, md: 5 }, display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+    >
+      <Card sx={{ width: { xs: '100%', md: '70%' }, mx: 'auto' }}>
+        <Box sx={{ p: { xs: 2, md: 5 }, textAlign: 'start' }}>
+          <Typography
+            variant="h2"
+            component="h2"
+            sx={{ mb: { xs: 5, md: 10 }, textAlign: 'center' }}
+          >
             {t('hakeemna medical blog')}
           </Typography>
 
           {/* Search and filter section */}
-          <Box sx={{ display: 'flex', mb: 5 }}>
+          <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, mb: 5 }}>
             <TextField
               label={t('Search')}
               variant="outlined"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              sx={{ width: '25%', mr: 3 }}
+              sx={{ width: { xs: '100%', md: '25%' }, mr: { md: 3 }, mb: { xs: 2, md: 0 } }}
             />
             <TextField
               select
               label={t('Filter by category')}
               value={filterBy}
               onChange={(e) => setFilterBy(e.target.value)}
-              sx={{ width: '25%' }}
+              sx={{ width: { xs: '100%', md: '25%' } }}
             >
-              <MenuItem value="">{t('All categorys')}</MenuItem>
+              <MenuItem value="">{t('All categories')}</MenuItem>
               {Data?.map((category, index) => (
                 <MenuItem key={index} value={category?._id}>
-                  {category?.[t("name_english")]}
+                  {category?.[t('name_english')]}
                 </MenuItem>
               ))}
             </TextField>
@@ -101,82 +108,68 @@ export default function Blogs() {
           {/* Blog Cards */}
           <Grid
             container
+            spacing={3}
             sx={{
-              display: 'grid',
-              gridTemplateColumns: { md: '1fr 1fr 1fr', xs: '1fr' },
-              gap: 2,
-              justifyContent: 'start',
-              alignItems: 'start',
+              gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr', md: '1fr 1fr 1fr' },
+              justifyContent: 'center',
             }}
           >
             {filteredBlogs?.map((blog, index) => (
-              <Card
-                key={index}
-                sx={{
-                  position: 'relative',
-                  overflow: 'hidden',
-                  mb: 3,
-                  width: '75%',
-                  height: '300px',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Image
-                  src={blog?.file}
-                  alt={blog.title}
-                  sx={{ width: '100%', height: '100%', objectFit: 'cover' }}
-                />
-                <Box sx={{ p: 2,maxHeight:150 }}>
-                  <Link
-                    sx={{ color: 'gray' }}
-                    href={paths.pages.doctor(
-                      `${blog.user?.employee?.employee_engagements?.[
-                        user?.employee?.selected_engagement
-                      ]?._id
-                      }_${blog.user?.employee?.name_english?.replace(/ /g, '_')}`
-                    )}
-                  >
-                    {blog.user?.employee?.name_english}
-                  </Link>
-                  <Typography sx={{ color: 'gray' }}>{blog.title}</Typography>
-                  <Typography
-                    dangerouslySetInnerHTML={{
-                      __html: formatTextWithLineBreaks(blog.topic),
-                    }}
-                    sx={{
-                      mb: 3,
-                      mt: 3,
-                      fontWeight: 'bold',
-                      transition: 'all 0.5s ease-in-out',
-                    }}
-                  />
-                  <Typography sx={{ color: 'gray', mt: 2 }}>
-                    {fDateAndTime(blog.created_at)}
-                  </Typography>
-                </Box>
-                <Button
-                  variant="contained"
+              <Grid item key={index} xs={12} sm={6} md={4}>
+                <Card
                   sx={{
-                    bgcolor: 'success.main',
-                    mt: 3,
+                    position: 'relative',
+                    overflow: 'hidden',
+                    height: '400px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
                   }}
-                  onClick={() => router.push(paths.pages.BlogsView(blog?._id))}
                 >
-                  {t('View')}
-                </Button>
-              </Card>
+                  <Image
+                    src={blog?.file}
+                    alt={blog.title}
+                    sx={{ width: '100%', height: '200px', objectFit: 'cover' }}
+                  />
+                  <Box sx={{ p: 2 }}>
+                    <Link
+                      sx={{ color: 'gray' }}
+                      href={paths.pages.doctor(
+                        `${
+                          blog?.user?.employee?.employee_engagements[0]
+                        }_${blog?.user?.employee?.name_english?.replace(/ /g, '_')}`
+                      )}
+                    >
+                      {blog.user?.employee?.name_english}
+                    </Link>
+                    <Typography sx={{ color: 'gray', fontWeight: 'bold', mt: 1 }}>
+                      {blog.title}
+                    </Typography>
+                    <Typography
+                      dangerouslySetInnerHTML={{
+                        __html: formatTextWithLineBreaks(blog.topic),
+                      }}
+                      sx={{ mt: 1, transition: 'all 0.5s ease-in-out' }}
+                    />
+                    <Typography sx={{ color: 'gray', mt: 2 }}>
+                      {fDateAndTime(blog.created_at)}
+                    </Typography>
+                  </Box>
+                  <Button
+                    variant="contained"
+                    sx={{ bgcolor: 'success.main', mx: 2, mb: 2 }}
+                    onClick={() => router.push(paths.pages.BlogsView(blog?._id))}
+                  >
+                    {t('View')}
+                  </Button>
+                </Card>
+              </Grid>
             ))}
           </Grid>
+
+          {/* Load More Button */}
           <Stack alignItems="center" sx={{ mt: 8, mb: { xs: 10, md: 15 } }}>
-            {/* <Button
-            size="large"
-            variant="outlined"
-            startIcon={<Iconify icon="svg-spinners:12-dots-scale-rotate" width={24} />}
-          >
-            Load More
-          </Button> */}
+            {/* Placeholder for Load More button if needed */}
           </Stack>
         </Box>
       </Card>
