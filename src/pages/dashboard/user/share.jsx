299e28543d0a1871c4e7c610a47b42c91bdea6d2
mohammad <mhd.keath@gmail.com>
2025-03-04 08:40:13 +0300

    vedio chat using webrtc

--- src/pages/dashboard/user/share.jsx
+++ src/pages/dashboard/user/share.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from 'react-helmet-async';
+import WebRTCComponent from 'src/components/vedio-call/webRTC';
 
 import { useTranslate } from 'src/locales';
 
@@ -17,6 +18,7 @@ export default function Sharedoctorna() {
       </Helmet>
 
       <Share />
+      {/* <WebRTCComponent /> */}
     </>
   );
 }
