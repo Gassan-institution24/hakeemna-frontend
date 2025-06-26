b27d5416ec20cbb9ff2d420222d495147e657c53
MarioISTQ <mario_nan@istqservers-eg.com>
2025-06-26 16:12:19 +0300

    feat(US patient): Add one US patient
    - Created two tabs between adding multiple or one patient
    - Created the add One patient form
    - added more arabic translation to ar.json

---
+++ src/sections/unit-service/patients/new-patient/add-one-patient.jsx
@@ -0,0 +1,223 @@
+import * as Yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import LoadingButton from '@mui/lab/LoadingButton';
+import Typography from '@mui/material/Typography';
+import { paths } from 'src/routes/paths';
+import { useRouter } from 'src/routes/hooks';
+import axiosInstance, { endpoints } from 'src/utils/axios';
+import { useAuthContext } from 'src/auth/hooks';
+import { useGetUSActiveWorkGroups } from 'src/api';
+import { useSnackbar } from 'src/components/snackbar';
+import { useLocales, useTranslate } from 'src/locales';
+
+// Components
+import FormProvider, {
+  RHFTextField,
+  RHFSelect,
+} from 'src/components/hook-form';
+import { MenuItem } from '@mui/material';
+
+// ----------------------------------------------------------------------
+export default function AddOnePatient() {
+  const router = useRouter();
+  const { t } = useTranslate();
+  const { currentLang } = useLocales();
+  const curLangAr = currentLang.value === 'ar';
+  const { user } = useAuthContext();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const employee = user?.employee?.employee_engagements?.[user.employee.selected_engagement];
+  const { workGroupsData } = useGetUSActiveWorkGroups(employee?.unit_service?._id);
+
+  const handleEnglishInputChange = (event) => {
+    const englishRegex = /^[a-zA-Z0-9\s,@#$!*_\-&^%.()]*$/; // Only allow English chars
+    if (englishRegex.test(event.target.value)) {
+      methods.setValue(event.target.name, event.target.value, { shouldValidate: true });
+    }
+  };
+
+  const handleArabicInputChange = (event) => {
+    const arabicRegex = /^[\u0600-\u06FF0-9\s!@#$%^&*_\-().]*$/; // Only allow Arabic chars
+    if (arabicRegex.test(event.target.value)) {
+      methods.setValue(event.target.name, event.target.value, { shouldValidate: true });
+    }
+  };
+
+  const NewPatientSchema = Yup.object().shape({
+    name_english: Yup.string()
+      .required(t('required field'))
+      .test('is-three-words', t('must be at least three words'), (value) =>
+        value ? value.trim().split(/\s+/).length === 3 : false
+      ),
+    name_arabic: Yup.string()
+      .required(t('required field'))
+      .test('is-three-words', t('must be at least three words'), (value) =>
+        value ? value.trim().split(/\s+/).length === 3 : false
+      ),
+    mobile_num1: Yup.string().required(t('required field')),
+    email: Yup.string().email(t('invalid email')).required(t('required field')),
+    gender: Yup.string()
+      .oneOf(['male', 'female'], t('required field'))
+      .required(t('required field')),
+    birth_date: Yup.date().required(t('required field')),
+    marital_status: Yup.string()
+      .oneOf(['single', 'married', 'widowed', 'separated', 'divorced'],t('required field'))
+      .required(t('required field')),
+    file_code: Yup.string().required(t('required field')),
+    work_group: Yup.string().required(t('work group is required')),
+  });
+
+  const defaultValues = {
+    name_english: '',
+    name_arabic: '',
+    mobile_num1: '',
+    email: '',
+    gender: '',
+    birth_date: null,
+    marital_status: '',
+    file_code: '',
+    work_group: '',
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(NewPatientSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    handleSubmit,
+    setValue,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onSubmit = async (data) => {
+    try {
+      await axiosInstance.post(endpoints.usPatients.addOne, [
+        {
+          ...data,
+          unit_service: employee?.unit_service?._id,
+          employee: employee?._id,
+        },
+      ]);
+
+      reset();
+      enqueueSnackbar(t('patient created successfully'));
+      router.push(paths.unitservice.patients.all);
+    } catch (error) {
+      enqueueSnackbar(error.message || t('failed to create patient'), { variant: 'error' });
+    }
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)} >
+      <Card sx={{ pt: "24px",px:"40px",pb:"16px", width: '100%' }}>
+        {/* Work Group Selection */}
+        <Typography variant="subtitle1" gutterBottom>
+            {t('work group')}
+          </Typography>
+
+        <Box
+          rowGap={3}
+          columnGap={2}
+          display="grid"
+          gridTemplateColumns={{
+            xs: 'repeat(1, 1fr)',
+            sm: 'repeat(2, 1fr)',
+          }}
+          sx={{ mb: 3 }}>
+          <RHFSelect
+            name="work_group"
+            SelectProps={{ native: false }}
+            sx={{ }}
+          >
+            {workGroupsData?.map((group) => (
+              <MenuItem key={group._id} value={group._id}>
+                {curLangAr ? group.name_arabic : group.name_english}
+              </MenuItem>
+            ))}
+          </RHFSelect>
+        </Box>
+
+        {/* Patient Info Section */}
+        <Typography variant="h6" gutterBottom>
+          {t('patient information')}
+        </Typography>
+
+        <Box
+          rowGap={3}
+          columnGap={2}
+          display="grid"
+          gridTemplateColumns={{
+            xs: 'repeat(1, 1fr)',
+            sm: 'repeat(2, 1fr)',
+          }}
+        >
+          {/* Name English & Arabic */}
+          <RHFTextField
+            name="name_english"
+            label={t('name english')}
+            helperText={t('must be at least three words')}
+            onChange={(e) => {handleEnglishInputChange(e);}}
+          />
+          <RHFTextField
+            name="name_arabic"
+            label={t('name arabic')}
+            helperText={t('must be at least three words')}
+            onChange={(e) => {handleArabicInputChange(e);}}
+          />
+
+          {/* Mobile Number & Email */}
+          <RHFTextField name="mobile_num1" label={t('mobile number')} />
+          <RHFTextField name="email" label={t('email')} />
+
+          {/* Gender & Birth Date */}
+          <RHFSelect name="gender" label={t('gender')}>
+            <MenuItem lang="ar" value="male">
+              {t('male')}
+            </MenuItem>
+            <MenuItem lang="ar" value="female">
+              {t('female')}
+            </MenuItem>
+          </RHFSelect>
+          <RHFTextField
+            name="birth_date"
+            type="date"
+            label={t('birth date')}
+            InputLabelProps={{ shrink: true }}
+          />
+
+          {/* Marital Status & File Code */}
+          <RHFSelect name="marital_status" label={t('marital status')}>
+            <MenuItem lang="ar" value="single">
+              {t('Single')}
+            </MenuItem>
+            <MenuItem lang="ar" value="married">
+              {t('Married')}
+            </MenuItem>
+            <MenuItem lang="ar" value="widowed">
+              {t('Widowed')}
+            </MenuItem>
+            <MenuItem lang="ar" value="separated">
+              {t('Separated')}
+            </MenuItem>
+            <MenuItem lang="ar" value="divorced">
+              {t('Divorced')}
+            </MenuItem>
+          </RHFSelect>
+          <RHFTextField name="file_code" label={t('File code')} />
+        </Box>
+
+        <Stack alignItems="flex-end" sx={{ mt: 3 }}>
+          <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+            {t('Create a new patient')}
+          </LoadingButton>
+        </Stack>
+      </Card>
+    </FormProvider>
+  );
+}
--- src/sections/unit-service/patients/view/new-patient.jsx
+++ src/sections/unit-service/patients/view/new-patient.jsx
@@ -1,5 +1,9 @@
 import Container from '@mui/material/Container';
 
+import { useState, useCallback } from 'react';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+
 import { paths } from 'src/routes/paths';
 
 import { useTranslate } from 'src/locales';
@@ -7,11 +11,34 @@ import { useTranslate } from 'src/locales';
 import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
 
 import TableManyNewForm from '../new-patient/add-many-patients';
+import AddOnePatient from '../new-patient/add-one-patient';
 
 // ----------------------------------------------------------------------
 
 export default function TableCreateView() {
   const { t } = useTranslate();
+
+  const [currentTab, setCurrentTab] = useState(t('Add mutliple patients'));
+
+  const handleChangeTab = useCallback((event, newValue) => {
+    setCurrentTab(newValue);
+  }, []);
+
+  const HistoryTabsList = [t('Add mutliple patients'), t('Add a single patient')];
+
+  const renderTabs = (
+    <Tabs
+      value={currentTab}
+      onChange={handleChangeTab}
+      sx={{
+        mb: { xs: 3, md: 5 },
+      }}
+    >
+      {HistoryTabsList.map((tab, idx) => (
+        <Tab key={idx} iconPosition="end" value={tab} label={tab} />
+      ))}
+    </Tabs>
+  );
 
   return (
     <Container maxWidth="xl">
@@ -33,7 +60,11 @@ export default function TableCreateView() {
         }}
       />
 
-      <TableManyNewForm />
+      {renderTabs}
+      {currentTab === t('Add mutliple patients') && <TableManyNewForm />}
+      {currentTab === t('Add a single patient') && <AddOnePatient />}
+
+
     </Container>
   );
 }
